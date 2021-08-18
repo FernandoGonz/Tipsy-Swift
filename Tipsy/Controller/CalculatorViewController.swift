005eced669c554e514ffaad82a4c7f353320aeb3
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-08-18 12:52:39 -0500

    The Storyboard files are done

---
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Fernando González on 18/08/21.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
+
