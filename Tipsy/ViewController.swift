4544fdac9ff584f2b912e580d4cafd795de07e87
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-08-18 10:09:47 -0500

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Fernando González on 18/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
